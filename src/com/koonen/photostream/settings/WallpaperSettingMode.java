3a4bccbb4ea57eece5b8aad881bd554356edc3ae
Glick Yury <yury.glick@axhive.com>
2009-03-23 16:41:41 +0600

    new feature: wallpaper setting mode

---
+++ src/com/koonen/photostream/settings/WallpaperSettingMode.java
@@ -0,0 +1,30 @@
+package com.koonen.photostream.settings;
+
+/**
+ * 
+ * @author glick
+ *
+ */
+public enum WallpaperSettingMode {
+
+	ORIGINAL_MODE("original"),
+
+	STRETCH_MODE("stretch"),
+
+	AUTO_MODE("auto");
+
+	private final String name;
+
+	private WallpaperSettingMode(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
